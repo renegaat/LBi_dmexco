de90b2da1c8fd23b98d22129bddacc10595b273f
renegaat <joross@dehhe6530-1.lbi.local>
2014-08-27 12:14:03 +0200

    final commit

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,7 @@
-var TiBeacons = require('de.lbi.beaconlocator');
+var TiBeacons = require('de.lbi.beaconLocator');
 
 Alloy.Collections.iBeacon.fetch();
+
 
 function enterRegion(e) {
 	alert(e);
@@ -12,22 +13,21 @@ function exitRegion(e) {
 
 	var model = ensureModel(e);
 	Alloy.Collections.iBeacon.remove(model);
-
     TiBeacons.stopRangingForBeacons(e);
 }
+
 function updateRanges(e) {
 	Ti.API.trace(e);
 }
+
 function handleProximity(e) {
 	Ti.API.info(e);
 	
-	var model = ensureModel(e);
-	
+	var model = ensureModel(e);	
 	model.set("proximity", e.proximity);
 }
 
-function ensureModel(e) {
-	
+function ensureModel(e) {	
 	var atts = {
 		id: e.uuid+" "+e.major+" "+e.minor,
 		identifier: e.identifier,
@@ -46,9 +46,8 @@ function ensureModel(e) {
 	}
 	else {
 		model = models[0];
-Ti.API.info("found model "+models[0].get("identifier"));	
+	Ti.API.info("found model "+models[0].get("identifier"));	
 	}
-
 	return model;
 }
 
@@ -59,23 +58,11 @@ TiBeacons.addEventListener("exitedRegion", exitRegion);
 TiBeacons.addEventListener("beaconRanges", updateRanges);
 TiBeacons.addEventListener("beaconProximity", handleProximity);
 	
-
-function toggleAdvertising() {
-
-}
-
-function toggleMonitoring() {
-
-    if ($.monitoringSwitch.value) {
-        TiBeacons.startMonitoringForRegion({
-            uuid : "B9407F30-F5F8-466E-AFF9-25556B57FE6D",
-            identifier : "Estimote"
-        });
-
-    } else {
-		TiBeacons.stopMonitoringAllRegions();
-    }
-}
-
-
+	TiBeacons.startMonitoringForRegion({
+	    uuid : "B9407F30-F5F8-466E-AFF9-25556B57FE6D",
+	    identifier : "Estimote 1",
+	    major : "65185",
+	    minor : "25294"
+	});
+	
 $.win.open();
